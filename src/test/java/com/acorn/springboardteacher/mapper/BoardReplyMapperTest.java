e3e1c724918282de4e57f03567f36de6525ea76e
develckm <develckm@gmail.com>
2023-03-30 09:51:50 +0900

    댓글 테스트 진행

--- src/test/java/com/acorn/springboardteacher/mapper/BoardReplyMapperTest.java
+++ src/test/java/com/acorn/springboardteacher/mapper/BoardReplyMapperTest.java
@@ -2,6 +2,7 @@ package com.acorn.springboardteacher.mapper;
 
 import com.acorn.springboardteacher.dto.BoardReplyDto;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,17 +27,30 @@ class BoardReplyMapperTest {
 
     @Test
     void findByBrId() {
+        BoardReplyDto boardReply = boardReplyMapper.findByBrId(1);
+        System.out.println("boardReply = " + boardReply);
+        Assertions.assertNotNull(boardReply);
     }
 
     @Test
-    void insertOne() {
+    void insertUpdateDeleteOne() {
+        BoardReplyDto boardReply=new BoardReplyDto();
+        boardReply.setBId(6);
+        boardReply.setParentBrId(19); //대대댓글
+        boardReply.setUId("user08");
+        boardReply.setImgPath("테스트대대댓글이미지2");
+        boardReply.setContent("user08 19번 대댓글에 작성한 대대댓글!! 우우~~~");
+        int insertOne = boardReplyMapper.insertOne(boardReply);
+        System.out.println("boardReply = " + boardReply);
+
+        boardReply.setImgPath("수정대댓글이미지~");
+        boardReply.setContent("내용을 수정하는 테스트 진행중");
+
+        int updateOne = boardReplyMapper.updateOne(boardReply);
+
+        int deleteOne = boardReplyMapper.deleteOne(boardReply.getBrId());
+
+        Assertions.assertEquals(insertOne+updateOne+deleteOne,3);
     }
 
-    @Test
-    void updateOne() {
-    }
-
-    @Test
-    void deleteOne() {
-    }
 }
