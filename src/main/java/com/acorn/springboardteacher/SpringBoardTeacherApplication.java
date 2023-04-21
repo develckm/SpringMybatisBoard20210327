dbdd897458c11aeecbdee62b602502bb276bc60d
develckm <develckm@gmail.com>
2023-04-21 14:15:30 +0900

    비밀번호 내역만 제외하도록 변경

--- src/main/java/com/acorn/springboardteacher/SpringBoardTeacherApplication.java
+++ src/main/java/com/acorn/springboardteacher/SpringBoardTeacherApplication.java
@@ -2,8 +2,11 @@ package com.acorn.springboardteacher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources(@PropertySource("/env.properties"))
 public class SpringBoardTeacherApplication {
 
 	public static void main(String[] args) {
